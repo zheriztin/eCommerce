f01040322d8784b07967e05d2f3e474075e3b86f
christin <zheriztin@gmail.com>
2021-10-21 23:42:05 +0700

    update transaction and checkout

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,37 +1,12 @@
 const { Transaction } = require('../models')
+// const nodemailer = require('nodemailer')
 
 module.exports = class Controller {
   static listTransactions(req, res) {
     console.log("MASUK LIST TRANSACTION", req.session);
     const {user:{ id: UserId}, role} = req.session
 
-    Transaction.findAll({ where: {UserId}, include: {all: true, nested: true} })
-    .then(data => {
-      // IF TRANSACTION ADA
-      if(data) {
-        console.log("masuk di IF TRANSACTION ADA");
-        const bulkUpdateInput = data.map(transaction => {
-          let total = 0
-          transaction.Orders.forEach(order => {
-            total += order.subTotal
-          })
-          return { total}
-        })
-        return Transaction.bulkUpdate(bulkUpdateInput, { where: {UserId}, returning: true})
-      } else {
-        console.log("ini di else transatcion FINDONE");
-      }
-
-    })
-    .then(data => {
-      //IF TRANSACTION ADA
-      if(data) {
-        console.log("MASUK DI IF TRANSACTION UPDATE");
-        return Transaction.findAll({ where: {UserId}, include: {all: true, nested: true} })
-      } else {
-        console.log("ini di ELSE TRANSACTION UDPATE");
-      }
-    })
+    Transaction.findAll({ where: {UserId} })
     .then(data => {
       console.log("masuk then terakhisr");
       res.render('transaction', {data,role})
@@ -42,4 +17,57 @@ module.exports = class Controller {
 
     })
   }
-}
+
+  static getTransactionDetail(req,res) {
+    const {id} = req.params
+    
+    Transaction.findByPk(id, {include: {all: true, nested: true}} )
+    .then(data => {
+      res.render('transactionDetail', {data})
+
+    })
+    .catch(error => {
+      res.send(error)
+    })
+  }
+
+
+
+  // static sendInvoice (req,res) {
+  //   const { 
+  //     user: {
+  //       id: UserId,
+  //       name,
+  //       email
+  //     }
+  //   } = req.session
+  //   console.log("MASUK TRANSACITON  SEND INVOCIE");
+  //   let transporter = nodemailer.createTransport({
+  //     service: 'gmail',
+  //     secure: false,
+  //     auth: {
+  //       user: "phase1ecommerce@gmail.com",
+  //       pass: "Phase123@ecommerce"
+  //     },
+  //     sendMail:true
+  //   })
+
+  //   transporter.sendMail({
+  //     from: "'Shopping Mania' phase1ecommerce@gmail.com",
+  //     to: "mwitaryo@gmail.com",
+  //     subject: "Invoice",
+  //     text: "DAfatar bbelaanjarfasfasfsa",
+  //     html: '<h1> THANK YOUUUU </h1>'
+  //   })
+  //   .then(info => {
+  //     console.log("Message sent: %s", info.messageId);
+  //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  //     console.log("masuk thennnnnnn>>>>>>>>>>>>>>>>>>>>>>><<<<<<<");
+  //     res.send(nodemailer.getTestMessageUrl(info))
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   })
+  // }
+  }
+
--- routes/cart.js
+++ routes/cart.js
@@ -2,19 +2,6 @@ const { addToCart, getCart, checkout } = require('../controllers/cart')
 
 const router = require('express').Router()
 
-const isLoginMiddleware = function (req, res, next)  {
-  if (req.session.isLogin) {
-    console.log("ini");
-    next()
-  } else {
-    console.log("masiukkkk ke else ?????");
-    // redirect ke Landing Page
-    res.json("nonoono")
-    // res.redirect('/')
-  }
-
-}
-
 router.get('/', getCart )
 router.post('/add', addToCart)
 router.get('/checkout', checkout)
